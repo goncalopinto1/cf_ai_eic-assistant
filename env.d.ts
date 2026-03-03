4400a0e82ab84806024bc7cb2ae3788021238028
Gonçalo Pinto <goncalo.luis.pinto@gmail.com>
2026-03-03 21:58:03 +0000

    first commit

--- env.d.ts
+++ env.d.ts
@@ -8,6 +8,7 @@ declare namespace Cloudflare {
 	interface Env {
 		AI: Ai;
 		ChatAgent: DurableObjectNamespace<import("./src/server").ChatAgent>;
+		VECTORIZE: Vectorize
 	}
 }
 interface Env extends Cloudflare.Env {}
